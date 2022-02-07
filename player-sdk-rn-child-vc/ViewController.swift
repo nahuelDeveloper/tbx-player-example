a091098058000592aa721b6cdfe7e27455b761b2
Nahuel Roldan <nroldan@toolboxtve.com>
2022-02-07 11:09:06 -0300

    Initial Commit

---
+++ player-sdk-rn-child-vc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  player-sdk-rn-child-vc
+//
+//  Created by Mobile Team on 07/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

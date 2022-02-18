d3a8f4e0f7221dec7de9ddd95dc15f760776f385
Nahuel Roldan <nroldan@toolboxtve.com>
2022-02-18 10:52:32 -0300

    Changed to tab bar to test issue when switching between tabs

--- player-sdk-rn-child-vc/ViewController.swift
+++ player-sdk-rn-child-vc/ViewController.swift
@@ -11,12 +11,21 @@ import TBXPlayerSDK
 let contentID = "61d35ee0a4cc292e2f37eabb"
 let token = "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJUb29sYm94IERpZ2l0YWwgU0EiLCJhdWQiOiJ1bml0eS50YnhhcGlzLmNvbSIsImlhdCI6MTY0NTAxOTY5MywiZXhwIjoxNjQ1MTkyNDkzLCJjb3VudHJ5IjoiTVgiLCJsYW5ndWFnZSI6ImVzIiwiY2xpZW50IjoiM2ExYTA3MzY0MDkxZTdhZDU3ODgxMzNhYWYyNjBmYWEiLCJkZXZpY2UiOiJjYjI4ZGU3Njc4ZjdhMjA3YzIyMWIwM2RmNTM1YmUwY2FlY2E2ZGVjIiwic3Vic2NyaWJlciI6IjEwMDAwMDI3MyIsImluZGV4IjoiNTg2ZDEyNzhkMWVhNTgwY2Q3NjNmNGZmIiwiY3VzdG9tZXIiOiI1NTY3NWVlNmRlY2NmZWFjMzNkMDU3MmIiLCJwcm9maWxlIjoiNjE1Yzk5MTdlYTE5MGYxNDAwMWVkZjVkIiwibWF4UmF0aW5nIjoyfQ.jtCMWMlhSXHDGZYxJZ3Qs7ElIbrMBIMYTkSzTNMNqac"
 
+let contentID2 = "61804531a4cc292e2f2e39a5"
+let networkID = "discoverykids"
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        TBXPlayerManager.shared.removePlayerEmbedded()
     }
     
     @IBOutlet weak var playerContainerView: UIView!
@@ -37,6 +46,11 @@ class ViewController: UIViewController {
         TBXPlayerManager.shared.preparePlayer(builder, with: getTopController(), on: self, containerView: playerContainerView)
         
         TBXPlayerManager.shared.startPlayer()
+    }
+    
+    @IBAction func changeMediaSource(_ sender: Any) {
+        TBXPlayerManager.shared.changeMediaSource(
+            ["contentId": contentID2])
     }
 }
 
